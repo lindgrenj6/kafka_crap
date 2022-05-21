4b46b4705ee7403f4059a7d3b3dee8a8a6b71118
Jacob Lindgren <jlindgre@redhat.com>
2022-05-20 20:19:44 -0500

    exit cleanly when consuming messages

--- consume_messages.go
+++ consume_messages.go
@@ -5,33 +5,50 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/segmentio/kafka-go"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	topic := flag.String("topic", "testtopic", "the topic to consume")
 	flag.Parse()
 
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
 	r := kafka.NewReader(kafka.ReaderConfig{
 		Brokers: []string{"localhost:9092"},
 		Topic:   *topic,
+		GroupID: "myreader",
 	})
-	defer r.Close()
+	go func() {
+		for {
+			msg, err := r.ReadMessage(context.Background())
+			if err != nil {
+				fmt.Printf("ERROR: %v", err)
+				os.Exit(1)
+			}
 
-	for {
-		msg, err := r.ReadMessage(context.Background())
-		if err != nil {
-			fmt.Printf("ERROR: %v", err)
-			os.Exit(1)
-		}
+			for _, h := range msg.Headers {
+				fmt.Printf(`{"key": %q, "value": %q}`, h.Key, string(h.Value))
+				fmt.Println()
+			}
 
-		for _, h := range msg.Headers {
-			fmt.Printf(`{"key": %q, "value": %q}`, h.Key, string(h.Value))
+			fmt.Println(string(msg.Value))
 			fmt.Println()
 		}
+	}()
 
-		fmt.Println(string(msg.Value))
-		fmt.Println()
-	}
+	<-sigs
+	go func() {
+		r.Close()
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
